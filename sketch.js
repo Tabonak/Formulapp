23965734ca6682557c6e8c8b3da634be702e684a
Tabonak <Tabonak@yandex.ru>
2026-03-05 23:19:42 +0300

    Добавлена возможность движения графика, масштаб, зум, высчитывание графика формулами (evaluateFormula). Добавлено отображение координат текщего пололжения мыши внутри холста. Убран ползунок.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,29 @@
 let slider;
+const SCALE = 50;
+let offsetX = 0;
+let offsetY = 0;
+let zoom = 1;
+
 
 function setup() {
   createCanvas(600, 400);
-  slider = document.getElementById("slider");
 }
 
 function draw() {
   background(255);
 
-  let a = parseFloat(slider.value);
+  let expr = document.getElementById("formula").value;
+
+  // движение графика мышью
+  if (mouseIsPressed &&
+    mouseX >= 0 && mouseX <= width &&
+    mouseY >= 0 && mouseY <= height) {
+  offsetX += (mouseX - pmouseX) / SCALE;
+  offsetY -= (mouseY - pmouseY) / SCALE;
+ }
+ 
+
+  push();
 
   //Сдвинули координаты
   translate(width / 2, height / 2);
@@ -20,11 +35,11 @@ function draw() {
 
   //Сетка
   stroke(220);
-  for (let x = -width / 2; x <= width / 2; x += 50) {
+  for (let x = -width / 2; x <= width / 2; x += SCALE) {
   line(x, -height / 2, x, height / 2);
   }
 
-  for (let y = -height / 2; y <= height / 2; y += 50) {
+  for (let y = -height / 2; y <= height / 2; y += SCALE) {
   line(-width / 2, y, width / 2, y);
   }
 
@@ -33,12 +48,12 @@ function draw() {
   noStroke();
   textSize(11);
 
-  for (let x = -width / 2; x <= width / 2; x += 50) {
-  text(x, x - 10, 18);
+  for (let x = -width / 2; x <= width / 2; x += SCALE) {
+  text(x / SCALE, x - 10, 15);
   }
 
-  for (let y = -height / 2; y <= height / 2; y += 50) {
-  text(-y, 8, y);
+  for (let y = -height / 2; y <= height / 2; y += SCALE) {
+  text(-y / SCALE, 5, y);
   }
 
   //Крестик посередине
@@ -47,11 +62,11 @@ function draw() {
   line(0, -5, 0, 5);
 
   //Риски
-  for (let x = -width / 2; x <= width / 2; x += 50) {
+  for (let x = -width / 2; x <= width / 2; x += SCALE) {
   line(x, -5, x, 5);
   }
 
-  for (let y = -height / 2; y <= height / 2; y += 50) {
+  for (let y = -height / 2; y <= height / 2; y += SCALE) {
   line(-5, y, 5, y);
   }
 
@@ -69,10 +84,61 @@ function draw() {
   noFill();
   beginShape();
 
-  for (let x = -300; x < 300; x += 1) {
-    let y = a * (x * x) / 100;
-    vertex(x, y);
+  for (let x = -width / 2; x < width / 2; x += 0.5) {
+
+  let unit = SCALE * zoom;
+
+  let xCoord = (x / unit) - offsetX;
+  let yCoord = evaluateFormula(expr, xCoord) + offsetY;
+
+  let y = yCoord * unit;
+
+  vertex(x, y);
   }
 
   endShape();
+
+  pop();
+
+  fill(0);
+  noStroke();
+  textSize(14);
+
+if (mouseX >= 0 && mouseX <= width &&
+    mouseY >= 0 && mouseY <= height) {
+
+  let unit = SCALE * zoom;
+
+  let mouseXCoord = (mouseX - width / 2) / unit;
+  let mouseYCoord = (height / 2 - mouseY) / unit;
+
+  text(`x = ${mouseXCoord.toFixed(2)}`, 10, 20);
+  text(`y = ${mouseYCoord.toFixed(2)}`, 10, 40);
+}
+
+}
+
+function keyPressed() {
+  if ((key === 'q' || key === 'й') &&
+      (keyIsDown(CONTROL) || keyIsDown(17))) {
+    offsetX = 0;
+    offsetY = 0;
+  }
+}
+
+function mouseWheel(event) {
+  if (event.delta > 0) {
+    zoom *= 0.9;
+  } else {
+    zoom *= 1.1;
+  }
+
+  // лимиты
+  zoom = constrain(zoom, 0.2, 5);
+
+  return false; // чтобы браузер не прокручивал страницу
+}
+
+function evaluateFormula(expr, x) {
+  return eval(expr);
 }
